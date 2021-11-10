ddbda7d5834332f2e7be086b5d37dcee30a21463
Guilherme Khoury <gui.khoury@usp.br>
2021-11-09 21:23:42 -0300

    Home page estilizada

--- src/modules/homePage/HomePage.page.js
+++ src/modules/homePage/HomePage.page.js
@@ -1,9 +1,12 @@
-import logo from "../../logo.svg";
-import "../../App.css";
+import { ReactComponent as Title } from "../../assets/TitleGreen.svg";
 import { useHistory } from "react-router-dom";
+import LoginTitleContainer from "../../components/login/LoginTitleContainer";
+import PageDefault from "../../components/PageDefault";
+import Colors from "../../constants/Colors";
+import LoginContainer from "../../components/login/LoginContainer";
+import LoginButton from "../../components/login/LoginButton";
 
 export const HomeScreen = () => {
-
   const history = useHistory();
 
   const handleAdminLogin = () => {
@@ -15,13 +18,18 @@ export const HomeScreen = () => {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Virtual Avalia</p>
-        <button onClick={handleAdminLogin}>Login Admin</button>
-        <button onClick={handleStudentLogin}>Login Student</button>
-      </header>
-    </div>
+    <PageDefault color={Colors.cream}>
+      <LoginTitleContainer>
+        <Title />
+      </LoginTitleContainer>
+      <LoginContainer>
+        <LoginButton color={Colors.primaryGreen} onClick={handleAdminLogin}>
+          Faça login como Professor
+        </LoginButton>
+        <LoginButton color={Colors.primaryGreen} onClick={handleStudentLogin}>
+          Faça login como Estudante
+        </LoginButton>
+      </LoginContainer>
+    </PageDefault>
   );
 };
