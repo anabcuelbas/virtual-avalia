5af554f9cf125018ed8399cacd5dbe797083599d
Guilherme Khoury <gui.khoury@usp.br>
2021-11-09 22:55:08 -0300

    fix imports

--- src/modules/admin/welcomeAdmin/WelcomeAdmin.page.js
+++ src/modules/admin/welcomeAdmin/WelcomeAdmin.page.js
@@ -1,6 +1,6 @@
-import Header from "../../../components/Header";
+import Header from "../../../components/header/Header";
 import PageDefault from "../../../components/PageDefault";
-import HeaderTitle from "../../../components/HeaderTitle";
+import HeaderTitle from "../../../components/header/HeaderTitle";
 
 export const WelcomeAdmin = () => {
   return (
